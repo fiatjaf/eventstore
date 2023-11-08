fed1abfe0a13caad529c062191792c263abf19be
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-11-09 01:18:53 +0900

    add kindtimeidx

--- sqlite3/init.go
+++ sqlite3/init.go
@@ -30,6 +30,7 @@ var ddls = []string{
 	`CREATE INDEX IF NOT EXISTS pubkeyprefix ON event(pubkey)`,
 	`CREATE INDEX IF NOT EXISTS timeidx ON event(created_at DESC)`,
 	`CREATE INDEX IF NOT EXISTS kindidx ON event(kind)`,
+	`CREATE INDEX IF NOT EXISTS kindtimeidx ON event(kind,created_at DESC)`,
 }
 
 func (b *SQLite3Backend) Init() error {
