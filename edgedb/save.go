89169747de9374470a9a1c786ed45d9fad622e12
TheRebelOfBabylon <therebelofbabylon@protonmail.com>
2024-07-27 15:38:54 -0400

    fixed missing module reference in edgeql statement

--- edgedb/save.go
+++ edgedb/save.go
@@ -8,7 +8,7 @@ import (
 
 func (b *EdgeDBBackend) SaveEvent(ctx context.Context, event *nostr.Event) error {
 	e := NostrEventToEdgeDBEvent(event)
-	query := "INSERT Event { eventId := <str>$eventId, pubkey := <str>$pubkey, createdAt := <datetime>$createdAt, kind := <int64>$kind, tags := array<json>$tags, content := <str>$content, sig := <str>$sig }"
+	query := "INSERT events::Event { eventId := <str>$eventId, pubkey := <str>$pubkey, createdAt := <datetime>$createdAt, kind := <int64>$kind, tags := array<json>$tags, content := <str>$content, sig := <str>$sig }"
 	args := map[string]interface{}{
 		"eventId":   event.ID,
 		"pubkey":    event.PubKey,
