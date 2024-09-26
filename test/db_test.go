ad942df85c77488b3367d3ef485f507cce747423
fiatjaf <fiatjaf@gmail.com>
2024-09-26 16:32:46 -0300

    add some more tests, should expand these later to be more specific.

--- test/db_test.go
+++ test/db_test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"context"
+	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"os"
 	"slices"
@@ -27,22 +29,45 @@ const (
 )
 
 func TestSliceStore(t *testing.T) {
-	runTestOn(t, &slicestore.SliceStore{})
+	t.Run("first", func(t *testing.T) {
+		runTestOn(t, &slicestore.SliceStore{})
+	})
+	t.Run("second", func(t *testing.T) {
+		runSecondTestOn(t, &slicestore.SliceStore{})
+	})
 }
 
 func TestLMDB(t *testing.T) {
 	os.RemoveAll(dbpath + "lmdb")
-	runTestOn(t, &lmdb.LMDBBackend{Path: dbpath + "lmdb"})
+	t.Run("first", func(t *testing.T) {
+		runTestOn(t, &lmdb.LMDBBackend{Path: dbpath + "lmdb"})
+	})
+	os.RemoveAll(dbpath + "lmdb")
+	t.Run("second", func(t *testing.T) {
+		runSecondTestOn(t, &lmdb.LMDBBackend{Path: dbpath + "lmdb"})
+	})
 }
 
 func TestBadger(t *testing.T) {
 	os.RemoveAll(dbpath + "badger")
-	runTestOn(t, &badger.BadgerBackend{Path: dbpath + "badger"})
+	t.Run("first", func(t *testing.T) {
+		runTestOn(t, &badger.BadgerBackend{Path: dbpath + "badger"})
+	})
+	os.RemoveAll(dbpath + "badger")
+	t.Run("second", func(t *testing.T) {
+		runSecondTestOn(t, &badger.BadgerBackend{Path: dbpath + "badger"})
+	})
 }
 
 func TestSQLite(t *testing.T) {
 	os.RemoveAll(dbpath + "sqlite")
-	runTestOn(t, &sqlite3.SQLite3Backend{DatabaseURL: dbpath + "sqlite"})
+	t.Run("first", func(t *testing.T) {
+		runTestOn(t, &sqlite3.SQLite3Backend{DatabaseURL: dbpath + "sqlite", QueryTagsLimit: 50})
+	})
+	os.RemoveAll(dbpath + "sqlite")
+	t.Run("second", func(t *testing.T) {
+		runSecondTestOn(t, &sqlite3.SQLite3Backend{DatabaseURL: dbpath + "sqlite", QueryTagsLimit: 50})
+	})
 }
 
 func TestPostgres(t *testing.T) {
@@ -53,16 +78,15 @@ func TestPostgres(t *testing.T) {
 		return
 	}
 	defer postgres.Stop()
-	runTestOn(t, &postgresql.PostgresBackend{
-		DatabaseURL: "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable",
-	})
+	runTestOn(t, &postgresql.PostgresBackend{DatabaseURL: "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable", QueryTagsLimit: 50})
+	runSecondTestOn(t, &postgresql.PostgresBackend{DatabaseURL: "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable", QueryTagsLimit: 50})
 }
+
+var ctx = context.Background()
 
 func runTestOn(t *testing.T, db eventstore.Store) {
 	err := db.Init()
 	require.NoError(t, err)
-
-	ctx := context.Background()
 
 	allEvents := make([]*nostr.Event, 0, 10)
 
@@ -287,4 +311,74 @@ func runTestOn(t *testing.T, db eventstore.Store) {
 				"'p' tag 3 query error")
 		}
 	}
+}
+
+func runSecondTestOn(t *testing.T, db eventstore.Store) {
+	db.Init()
+
+	for i := 0; i < 10000; i++ {
+		eTag := make([]byte, 32)
+		binary.BigEndian.PutUint16(eTag, uint16(i))
+
+		ref, _ := nostr.GetPublicKey(sk3)
+		if i%3 == 0 {
+			ref, _ = nostr.GetPublicKey(sk4)
+		}
+
+		evt := &nostr.Event{
+			CreatedAt: nostr.Timestamp(i*10 + 2),
+			Content:   fmt.Sprintf("hello %d", i),
+			Tags: nostr.Tags{
+				{"t", fmt.Sprintf("t%d", i)},
+				{"e", hex.EncodeToString(eTag)},
+				{"p", ref},
+			},
+			Kind: i % 10,
+		}
+		sk := sk3
+		if i%3 == 0 {
+			sk = sk4
+		}
+		evt.Sign(sk)
+		err := db.SaveEvent(ctx, evt)
+		require.NoError(t, err)
+	}
+
+	w := eventstore.RelayWrapper{Store: db}
+	pk3, _ := nostr.GetPublicKey(sk3)
+	pk4, _ := nostr.GetPublicKey(sk4)
+	eTags := make([]string, 20)
+	for i := 0; i < 20; i++ {
+		eTag := make([]byte, 32)
+		binary.BigEndian.PutUint16(eTag, uint16(i))
+		eTags[i] = hex.EncodeToString(eTag)
+	}
+
+	filters := make([]nostr.Filter, 0, 10)
+	filters = append(filters, nostr.Filter{Kinds: []int{1, 4, 8, 16}})
+	filters = append(filters, nostr.Filter{Authors: []string{pk3, nostr.GeneratePrivateKey()}})
+	filters = append(filters, nostr.Filter{Authors: []string{pk3, nostr.GeneratePrivateKey()}, Kinds: []int{3, 4}})
+	filters = append(filters, nostr.Filter{})
+	filters = append(filters, nostr.Filter{Limit: 20})
+	filters = append(filters, nostr.Filter{Kinds: []int{8, 9}, Tags: nostr.TagMap{"p": []string{pk3}}})
+	filters = append(filters, nostr.Filter{Kinds: []int{8, 9}, Tags: nostr.TagMap{"p": []string{pk3, pk4}}})
+	filters = append(filters, nostr.Filter{Kinds: []int{8, 9}, Tags: nostr.TagMap{"p": []string{pk3, pk4}}})
+	filters = append(filters, nostr.Filter{Kinds: []int{9}, Tags: nostr.TagMap{"e": eTags}})
+	filters = append(filters, nostr.Filter{Kinds: []int{5}, Tags: nostr.TagMap{"e": eTags, "t": []string{"t5"}}})
+	filters = append(filters, nostr.Filter{Tags: nostr.TagMap{"e": eTags}})
+	filters = append(filters, nostr.Filter{Tags: nostr.TagMap{"e": eTags}, Limit: 50})
+
+	t.Run("filter", func(t *testing.T) {
+		for q, filter := range filters {
+			q := q
+			filter := filter
+			label := fmt.Sprintf("filter %d: %s", q, filter)
+
+			t.Run(fmt.Sprintf("q-%d", q), func(t *testing.T) {
+				results, err := w.QuerySync(ctx, filter)
+				require.NoError(t, err, filter)
+				require.NotEmpty(t, results, label)
+			})
+		}
+	})
 }
