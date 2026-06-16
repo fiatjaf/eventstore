b6284247667fd073f5708898baf81931ea6226f9
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-06-17 01:51:36 +0900

    Avoid copying MySQL backend

--- mysql/delete.go
+++ mysql/delete.go
@@ -6,7 +6,7 @@ import (
 	"github.com/nbd-wtf/go-nostr"
 )
 
-func (b MySQLBackend) DeleteEvent(ctx context.Context, evt *nostr.Event) error {
+func (b *MySQLBackend) DeleteEvent(ctx context.Context, evt *nostr.Event) error {
 	_, err := b.DB.ExecContext(ctx, "DELETE FROM event WHERE id = ?", evt.ID)
 	return err
 }
